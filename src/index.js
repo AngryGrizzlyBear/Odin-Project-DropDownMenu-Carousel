4b0523ae58a23fb0a34c27cb8a0ec5920ee75d65
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2025-05-21 09:11:48 -0700

    Created code that works for multiple dropdown menus

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './styles.css';
 import { setupDropdown } from './dropdown';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dropdowns = document.querySelectorAll('.dropdown');
-  dropdowns.forEach(setupDropdown);
+  document.querySelectorAll('.dropdown').forEach((dropdownElement) => {
+    setupDropdown(dropdownElement);
+  });
+
+  document.addEventListener('click', () => {
+    document.querySelectorAll('.dropdown-menu.visible').forEach((menu) => {
+      menu.classList.remove('visible');
+    });
+  });
 });
